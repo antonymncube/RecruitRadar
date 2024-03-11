47cdf7d1f1db3c3a97eb020261cf553236b2d406
Antony <antonymncube49@gmail.com>
2024-03-11 12:35:38 +0200

    Cli Error

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d05fb2a679d97183345606d24cad37ac4ec71f5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f2c0a431959d8eac79fedc79247509e49c7de98")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
