c910abe8eb7245dd6c55c9bd4c666e44395fbca7
Antony <antonymncube49@gmail.com>
2024-03-11 14:48:20 +0200

    updating migration

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  users: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get<any>('https://localhost:5001/api/User').subscribe({
+      next: (response) => {
+        this.users = response;
+      },
+      error: (error) => {
+        console.error(error);
+      },
+      complete: () => {
+        console.log("Request completed");
+      }
+    });
+  }
 }
